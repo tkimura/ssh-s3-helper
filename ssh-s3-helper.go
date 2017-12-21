7e85f37baf23ea09d51725ba5ac733a1c8be1ed7
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-12-22 01:45:18 +0900

    Add modeline

--- ssh-s3-helper.go
+++ ssh-s3-helper.go
@@ -1,50 +1,52 @@
+# vim: set syntax=go ff=unix fileencoding=utf-8 expandtab ts=4 sw=4:
+
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-	"log"
-	"github.com/zieckey/goini"
-	"os"
-	"regexp"
-	"fmt"
+    "io/ioutil"
+    "net/http"
+    "log"
+    "github.com/zieckey/goini"
+    "os"
+    "regexp"
+    "fmt"
 )
 
 
 func main() {
-	ini := goini.New()
-	err := ini.ParseFile("/etc/default/ssh-s3-helper")
-	if err != nil {
-		log.Fatal(err)
-	}
-	s3_region, s3_region_ok := ini.Get("s3_region")
-	s3_bucket, s3_bucket_ok := ini.Get("s3_bucket")
-	s3_prefix, s3_prefix_ok := ini.Get("s3_prefix")
+    ini := goini.New()
+    err := ini.ParseFile("/etc/default/ssh-s3-helper")
+    if err != nil {
+        log.Fatal(err)
+    }
+    s3_region, s3_region_ok := ini.Get("s3_region")
+    s3_bucket, s3_bucket_ok := ini.Get("s3_bucket")
+    s3_prefix, s3_prefix_ok := ini.Get("s3_prefix")
 
-	if len(os.Args) != 2 {
-		log.Fatal("usage: "+os.Args[0]+" [user]")
-	}
+    if len(os.Args) != 2 {
+        log.Fatal("usage: "+os.Args[0]+" [user]")
+    }
 
-	user := os.Args[1]
+    user := os.Args[1]
 
-	user_re := regexp.MustCompile("^[a-z_][a-z0-9_-]{0,31}$")
+    user_re := regexp.MustCompile("^[a-z_][a-z0-9_-]{0,31}$")
 
-	if user_re.MatchString(user) == false {
-		log.Fatal("illegal character")
-	}
+    if user_re.MatchString(user) == false {
+        log.Fatal("illegal character")
+    }
 
-	if s3_region_ok && s3_bucket_ok && s3_prefix_ok {
-		sshkey_url := "https://s3-"+s3_region+".amazonaws.com/"+s3_bucket+"/"+s3_prefix+user
+    if s3_region_ok && s3_bucket_ok && s3_prefix_ok {
+        sshkey_url := "https://s3-"+s3_region+".amazonaws.com/"+s3_bucket+"/"+s3_prefix+user
 
-		resp, err := http.Get(sshkey_url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Print(string(body))
-	}
+        resp, err := http.Get(sshkey_url)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer resp.Body.Close()
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Print(string(body))
+    }
 }
